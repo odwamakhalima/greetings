eeaa496f7e5d3507005c342fde7787f9b6800121
Odwa Makhalima <3652687@myuwc.ac.za>
2019-05-27 14:41:48 +0200

    adding files

---
+++ test/greet.test.js
@@ -0,0 +1,6 @@
+describe('function that checks number registration numbers' , function(){
+    it('It should return 3 because there are 3 registration numbers' , function(){
+        assert.equal(greet('jhjj'),'[pkojo');
+    });
+
+});
